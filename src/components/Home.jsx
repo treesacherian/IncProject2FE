7e5a391174598b0edc02c36d206f743ae9aec961
AndrewClark399 <andy396clark@hotmail.co.uk>
2024-03-01 16:36:55 +0000

    created shopping page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,15 @@
 function Home() {
     return ( 
         <div>
+            <h1>CCZone Homepage</h1>
+            <br/>
+            <p>The new way to buy your groceries from the comfort of your Sofa
+
+            </p>
+            <p>Price matched against all major supermarkets.</p>
+            <p>Here to help with the cost of living!</p>
+
+            
             
         </div>
      );
--- src/components/cart/CreateCart.jsx
+++ src/components/cart/CreateCart.jsx
@@ -1,42 +1,44 @@
 import axios from "axios";
 
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import CartStructure from "./CartStructure";
 
 function CreateCart() {
 
     const cartList = []
-        const [carts, setCarts] = useState([]);
-    function getCarts(){
+    const [carts, setCarts] = useState([]);
+    function getCarts() {
         axios.get("http://localhost:8080/cart/get")
-        .then((response) => {setCarts(response.data)})
-        .catch(console.log())}
-        useEffect(()=> {getCarts()},[])
-                for (const cart of carts){
-                    cartList.push(<CartStructure
-                        id={cart.id}
-                        item={cart.itemId} />
-                        
-                    )
-                
-                }
+            .then((response) => { setCarts(response.data) })
+            .catch(console.log())
+    }
+    useEffect(() => { getCarts() }, [])
+    for (const cart of carts) {
+        cartList.push 
+    (<CartStructure
+            id={cart.id}
+            item={cart.itemId} />
+
+        )
+
+    }
     function handleclick() {
         axios.post("http://localhost:8080/cart/create")
-            .then(response => {getCarts() })
+            .then(response => { getCarts() })
             .catch(err => console.error(err))
 
-            
+
     }
 
-    
+
 
 
     return (
         <div class="border border-primary p-2 mb-2 border-4" style={{ backgroundColor: "#295821", width: "80%" }}>
-            <button  style= {{width: "200px", height: "40px", margin: "5px", marginLeft: "5px", marginTop:"15px"}}type="button" onClick={handleclick}>Create New Cart</button>
+            <button style={{ width: "200px", height: "40px", margin: "5px", marginLeft: "5px", marginTop: "15px" }} type="button" onClick={handleclick}>Create New Cart</button>
             <h3>Carts</h3>
-           <div class="border border-primary p-2 mb-2 border-4" style={{ backgroundColor: "#295821", width: "80%" }}> {cartList}
-           </div>
+            <div class="border border-primary p-2 mb-2 border-4" style={{ backgroundColor: "#295821", width: "80%" }}> {cartList}
+            </div>
         </div>
     );
 }
